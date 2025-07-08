334d86a2e33bbbe0a5c54d267abe16e0a4a6d960
sarah <ibrahimsarah839@gmail.com>
2025-07-08 09:21:23 +0300

    pending interactive

--- MCV/Frontend/fetch_tasks/second_task/second.js
+++ MCV/Frontend/fetch_tasks/second_task/second.js
@@ -10,7 +10,8 @@ function getapi() {
         listHTML += `<li id="todo-${item.id}" class="list-group-item d-flex justify-content-between align-items-center">
             <span class="todo-title">${item.title}</span>
         <div class="d-flex gap-2">
-        <button class="btn btn-sm btn-danger" id="pending-${item.id}">Pending</button>
+        <button class="btn btn-sm btn-danger" id="pending-${item.id}" onclick="changebackground(${item.id})">Pending</button>
+
         <button class="btn btn-sm btn-success" onclick="markDone(${item.id})">Done</button>
       </div>
     </li>
@@ -31,4 +32,8 @@ function markDone(id) {
   if (pendingBtn) {
     pendingBtn.style.display = "none";
   }
+}
+function changebackground(id) {
+  const title = document.getElementById(`todo-${id}`);
+  title.style.backgroundColor = "red";
 }
