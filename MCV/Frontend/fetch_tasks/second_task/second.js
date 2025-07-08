cc163a2f9eee73c950e205cd39d035603bac32d6
sarah <ibrahimsarah839@gmail.com>
2025-07-08 09:14:43 +0300

    fetch_tasks

---
+++ MCV/Frontend/fetch_tasks/second_task/second.js
@@ -0,0 +1,34 @@
+function getapi() {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => {
+      const sliced = json.slice(0, 6);
+      console.log(sliced);
+
+      let listHTML = `<ul class="list-group list-group-flush">`;
+      sliced.forEach((item) => {
+        listHTML += `<li id="todo-${item.id}" class="list-group-item d-flex justify-content-between align-items-center">
+            <span class="todo-title">${item.title}</span>
+        <div class="d-flex gap-2">
+        <button class="btn btn-sm btn-danger" id="pending-${item.id}">Pending</button>
+        <button class="btn btn-sm btn-success" onclick="markDone(${item.id})">Done</button>
+      </div>
+    </li>
+    </div>
+    `;
+      });
+      listHTML += `</ul>`;
+
+      document.getElementById("todolist").innerHTML = listHTML;
+    })
+    .catch((err) => console.error("Fetch error:", err));
+}
+function markDone(id) {
+  const title = document.getElementById(`todo-${id}`);
+  title.style.backgroundColor = "rgb(104, 206, 104)";
+  title.style.textDecoration = "line-through";
+  const pendingBtn = document.getElementById(`pending-${id}`);
+  if (pendingBtn) {
+    pendingBtn.style.display = "none";
+  }
+}
